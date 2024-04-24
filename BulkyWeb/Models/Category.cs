ca2e3dfdfae439dfa82923ac5eda564882fbec45
Gaurav Shimpi <gaurav.shimpi@harbingergroup.com>
2024-04-24 15:54:00 +0530

    Add project files.

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        //if we set property name as ID or modelname followed by id 
+        //entity framework will consider it Primary key while making tables
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100)]
+        public int DisplayOrder { get; set; }
+    }
+}
